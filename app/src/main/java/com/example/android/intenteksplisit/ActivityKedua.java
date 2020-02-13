3afc45d9d032c089ccc64f2ec1434c576a9021dc
JosiKie <kiejosi12@gmail.com>
2020-02-13 20:21:49 +0700

    first commit

---
+++ app/src/main/java/com/example/android/intenteksplisit/ActivityKedua.java
@@ -0,0 +1,14 @@
+package com.example.android.intenteksplisit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityKedua extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kedua);
+    }
+}
